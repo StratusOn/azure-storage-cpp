3640cc9f44437cc3cc602911f63be71074ed874d
Raouché <mjalloul@gmail.com>
2019-06-27 22:17:32 -0700

    Added to the sample the flow for creating the incremental snapshot and generating a SAS URL for the snapshot.

--- Microsoft.WindowsAzure.Storage/samples/IncrementalSnapshotsGettingStarted/Application.cpp
+++ Microsoft.WindowsAzure.Storage/samples/IncrementalSnapshotsGettingStarted/Application.cpp
@@ -18,45 +18,80 @@
 #include "stdafx.h"
 #include "samples_common.h"
 
-#include <was/storage_account.h>
-#include <was/blob.h>
-
 #include <cpprest/containerstream.h>
 #include <cpprest/filestream.h>
 #include <cpprest/http_client.h>
 #include <cpprest/json.h>
 #include <cpprest/producerconsumerstream.h>
 #include <iostream>
+#include <iomanip>
 #include <sstream>
+#include <chrono>
+#include <thread>
 
-namespace azure { namespace authentication { namespace utility {
+namespace azure { namespace common { namespace helpers {
+	
+	const int DEFAULT_MAX_ITERATIONS = 20;
+	const int THREAD_SLEEP_IN_MILLISECONDS = 1000;
 
-	typedef std::wstring string_t;
-
-	pplx::task<web::json::value> http_call_async(
+	pplx::task<web::http::http_response> http_call_async(
 		const utility::string_t& uri,
 		const utility::string_t& body,
 		const web::http::method method,
 		const utility::string_t& content_type,
-		const web::http::http_headers& headers,
-		web::http::status_code& status_code)
+		const web::http::http_headers& request_headers)
 	{
 		web::http::client::http_client client(uri);
 
-		web::http::http_request request(web::http::methods::POST);
+		web::http::http_request request(method);
 		request.set_body(body, content_type);
-		request.headers() = headers;
-
-		return client.request(request).then([&status_code](web::http::http_response response)
+		request.headers() = request_headers;
+		
+		return client.request(request).then([](web::http::http_response response)
 			{
-				//ucout << _XPLATSTR("Response Status Code: ") << response.status_code() << std::endl;
-				//ucout << _XPLATSTR("Content type: ") << response.headers().content_type() << std::endl;
-				//ucout << _XPLATSTR("Content length: ") << response.headers().content_length() << std::endl;
-				status_code = response.status_code();
-
-				return response.extract_json().get();
+				return response;
 			});
 	}
+
+	web::json::value track_operation_status(
+		const utility::string_t& operation_uri,
+		const utility::string_t& access_token)
+	{
+		utility::string_t payload_body(_XPLATSTR(""));
+		utility::string_t content_type(_XPLATSTR("application/json"));
+
+		utility::string_t authorization_header(_XPLATSTR("Bearer "));
+		authorization_header.append(access_token);
+		web::http::http_headers request_headers;
+		request_headers.add(_XPLATSTR("Authorization"), authorization_header);
+
+		utility::string_t status(_XPLATSTR("Running"));
+		web::json::value output = web::json::value();
+		int max_iterations = DEFAULT_MAX_ITERATIONS;
+
+		do
+		{
+			ucout << _XPLATSTR("Waiting for operation to complete: #") << (DEFAULT_MAX_ITERATIONS - max_iterations) << std::endl;
+			std::this_thread::sleep_for(std::chrono::milliseconds(THREAD_SLEEP_IN_MILLISECONDS));
+
+			http_call_async(operation_uri, payload_body, web::http::methods::GET, content_type, request_headers).then(
+				[&status, &output](web::http::http_response response)
+				{
+					web::json::value json_object = response.extract_json().get();
+					status = json_object.at(_XPLATSTR("status")).as_string();
+
+					if (status == _XPLATSTR("Succeeded"))
+					{
+						output = json_object; // .at(_XPLATSTR("properties")).at(_XPLATSTR("output"));
+					}
+				}).wait();
+		} while ((status == _XPLATSTR("Running") || status == _XPLATSTR("InProgress")) && --max_iterations > 0);
+		
+		return output;
+	}
+}}} // namespace azure::common::helpers
+
+namespace azure { namespace authentication { namespace helpers {
 
 	pplx::task<utility::string_t> get_access_token_from_service_principal_async(
 		const utility::string_t& authority,
@@ -79,12 +114,11 @@ namespace azure { namespace authentication { namespace utility {
 		payload_body.append(url_encoded_resource);
 
 		utility::string_t content_type(_XPLATSTR("application/x-www-form-urlencoded"));
-		web::http::http_headers headers;
-		web::http::status_code status_code(web::http::status_codes::NotImplemented);
+		web::http::http_headers request_headers;
 
-		return http_call_async(auth_uri, payload_body, web::http::methods::POST, content_type, headers, status_code).then([&status_code](web::json::value json_object)
+		return azure::common::helpers::http_call_async(auth_uri, payload_body, web::http::methods::POST, content_type, request_headers).then([](web::http::http_response response)
 			{
-				return json_object.at(_XPLATSTR("access_token")).as_string();
+				return response.extract_json().get().at(_XPLATSTR("access_token")).as_string();
 			});
 	}
 
@@ -99,16 +133,119 @@ namespace azure { namespace authentication { namespace utility {
 
 		utility::string_t payload_body(_XPLATSTR(""));
 		utility::string_t content_type(_XPLATSTR("text/plain"));
-		web::http::http_headers headers;
-		headers.add(_XPLATSTR("Metadata"), _XPLATSTR("true"));
-		web::http::status_code status_code(web::http::status_codes::NotImplemented);
+		web::http::http_headers request_headers;
+		request_headers.add(_XPLATSTR("Metadata"), _XPLATSTR("true"));
 
-		return http_call_async(auth_uri, payload_body, web::http::methods::GET, content_type, headers, status_code).then([](web::json::value json_object)
+		return azure::common::helpers::http_call_async(auth_uri, payload_body, web::http::methods::GET, content_type, request_headers).then([](web::http::http_response response)
 			{
-				return json_object.at(_XPLATSTR("access_token")).as_string();
+				return response.extract_json().get().at(_XPLATSTR("access_token")).as_string();
 			});
 	}
-}}} // namespace azure::authentication::utility
+}}} // namespace azure::authentication::helpers
+
+namespace azure { namespace compute { namespace snapshots {
+
+	pplx::task<void> create_incremental_snapshot_async(
+		const utility::string_t& subscription_id,
+		const utility::string_t& resource_group_name,
+		const utility::string_t& snapshot_name,
+		const utility::string_t& region,
+		const utility::string_t& parent_disk_resource_uri,
+		const utility::string_t& access_token)
+	{
+		utility::string_t create_snapshot_uri(_XPLATSTR("https://management.azure.com/subscriptions/"));
+		create_snapshot_uri.append(subscription_id);
+		create_snapshot_uri.append(_XPLATSTR("/resourceGroups/"));
+		create_snapshot_uri.append(resource_group_name);
+		create_snapshot_uri.append(_XPLATSTR("/providers/Microsoft.Compute/snapshots/"));
+		create_snapshot_uri.append(snapshot_name);
+		create_snapshot_uri.append(_XPLATSTR("?api-version=2019-03-01"));
+
+		web::json::value json_payload;
+		json_payload[_XPLATSTR("type")] = web::json::value::string(_XPLATSTR("Microsoft.Compute/snapshots"));
+		json_payload[_XPLATSTR("name")] = web::json::value::string(snapshot_name);
+		json_payload[_XPLATSTR("location")] = web::json::value::string(region);
+		web::json::value json_payload_sku;
+		json_payload_sku[_XPLATSTR("name")] = web::json::value::string(_XPLATSTR("Standard_LRS"));
+		web::json::value json_payload_properties_creation_data;
+		json_payload_properties_creation_data[_XPLATSTR("createOption")] = web::json::value::string(_XPLATSTR("Copy"));
+		json_payload_properties_creation_data[_XPLATSTR("sourceResourceId")] = web::json::value::string(parent_disk_resource_uri);
+		web::json::value json_payload_properties;
+		json_payload_properties[_XPLATSTR("creationData")] = json_payload_properties_creation_data;
+		json_payload_properties[_XPLATSTR("incremental")] = web::json::value::boolean(_XPLATSTR("true"));
+		json_payload[_XPLATSTR("sku")] = json_payload_sku;
+		json_payload[_XPLATSTR("properties")] = json_payload_properties;
+		utility::stringstream_t stream;
+		json_payload.serialize(stream);
+		utility::string_t payload_body(stream.str());
+
+		utility::string_t content_type(_XPLATSTR("application/json"));
+
+		utility::string_t authorization_header(_XPLATSTR("Bearer "));
+		authorization_header.append(access_token);
+		web::http::http_headers request_headers;
+		request_headers.add(_XPLATSTR("Authorization"), authorization_header);
+		request_headers.add(_XPLATSTR("Content-Type"), _XPLATSTR("application/json"));
+
+		return azure::common::helpers::http_call_async(create_snapshot_uri, payload_body, web::http::methods::PUT, content_type, request_headers).then(
+			[access_token](web::http::http_response response)
+			{
+				utility::string_t async_operation_uri(response.headers()[_XPLATSTR("Azure-AsyncOperation")]);
+				if (async_operation_uri.empty())
+				{
+					return;
+				}
+
+				web::json::value output = azure::common::helpers::track_operation_status(async_operation_uri, access_token);
+				//ucout << _XPLATSTR("Track Operation_Status JSON Output: ") << output.serialize() << std::endl;
+			});
+	}
+
+	pplx::task<utility::string_t> generate_sas_url_async(
+		const utility::string_t& subscription_id,
+		const utility::string_t& resource_group_name,
+		const utility::string_t& snapshot_name,
+		const utility::string_t& access_token)
+	{
+		utility::string_t get_snapshot_access_uri(_XPLATSTR("https://management.azure.com/subscriptions/"));
+		get_snapshot_access_uri.append(subscription_id);
+		get_snapshot_access_uri.append(_XPLATSTR("/resourceGroups/"));
+		get_snapshot_access_uri.append(resource_group_name);
+		get_snapshot_access_uri.append(_XPLATSTR("/providers/Microsoft.Compute/snapshots/"));
+		get_snapshot_access_uri.append(snapshot_name);
+		get_snapshot_access_uri.append(_XPLATSTR("/beginGetAccess?api-version=2019-03-01"));
+
+		web::json::value get_snapshot_access_payload;
+		get_snapshot_access_payload[_XPLATSTR("access")] = web::json::value::string(_XPLATSTR("Read")); // TODO: Make "access" a parameter to this function.
+		get_snapshot_access_payload[_XPLATSTR("durationInSeconds")] = web::json::value::string(_XPLATSTR("3600")); // TODO: Make "durationInSeconds" a parameter to this function.
+		utility::stringstream_t stream;
+		get_snapshot_access_payload.serialize(stream);
+		utility::string_t payload_body(stream.str());
+
+		utility::string_t content_type(_XPLATSTR("application/json"));
+
+		utility::string_t authorization_header(_XPLATSTR("Bearer "));
+		authorization_header.append(access_token);
+		web::http::http_headers request_headers;
+		request_headers.add(_XPLATSTR("Authorization"), authorization_header);
+		request_headers.add(_XPLATSTR("Content-Type"), _XPLATSTR("application/json"));
+
+		return azure::common::helpers::http_call_async(get_snapshot_access_uri, payload_body, web::http::methods::POST, content_type, request_headers).then(
+			[access_token](web::http::http_response response)
+			{
+				utility::string_t async_operation_uri(response.headers()[_XPLATSTR("Azure-AsyncOperation")]);
+				if (async_operation_uri.empty())
+				{
+					return utility::string_t(_XPLATSTR(""));
+				}
+
+				web::json::value output = azure::common::helpers::track_operation_status(async_operation_uri, access_token);
+				//ucout << _XPLATSTR("Track Operation_Status JSON Output: ") << output.serialize() << std::endl;
+				utility::string_t sas_url(output.at(_XPLATSTR("properties")).at(_XPLATSTR("output")).at(_XPLATSTR("accessSAS")).as_string());
+				return sas_url;
+			});
+	}
+}}} // namespace azure::compute::snapshots
 
 namespace azure { namespace storage { namespace samples {
 
@@ -116,7 +253,16 @@ namespace azure { namespace storage { namespace samples {
     {
         try
         {
-			utility::string_t access_token = azure::authentication::utility::get_access_token_from_service_principal_async(aad_authority, aad_resource, aad_tenant_id, aad_application_id, aad_application_secret).get();
+			// 1. Get the access token.
+			utility::string_t access_token = azure::authentication::helpers::get_access_token_from_service_principal_async(aad_authority, aad_resource, aad_tenant_id, aad_application_id, aad_application_secret).get();
+			//ucout << _XPLATSTR("Access Token: ") << access_token << std::endl;
+
+			// 2. Create an incremental snapshot.
+			azure::compute::snapshots::create_incremental_snapshot_async(subscription_id, resource_group_name, snapshot_name, region, managed_disk_resource_uri, access_token).get();
+
+			// 3. Generate a SAS URL.
+			utility::string_t sas_url = azure::compute::snapshots::generate_sas_url_async(subscription_id, resource_group_name, snapshot_name, access_token).get();
+			ucout << _XPLATSTR("Snapshot SAS URL: ") << sas_url << std::endl;
         }
         catch (const std::exception& e)
         {
--- Microsoft.WindowsAzure.Storage/samples/SamplesCommon/samples_common.h
+++ Microsoft.WindowsAzure.Storage/samples/SamplesCommon/samples_common.h
@@ -24,19 +24,44 @@ namespace azure { namespace storage { namespace samples {
     // TODO: Put your account name and account key here
      utility::string_t storage_connection_string(_XPLATSTR("DefaultEndpointsProtocol=https;AccountName=myaccountname;AccountKey=myaccountkey"));
 
+	// ***********************************************************************
+	// ** Settings below are used in the Incremental Snapshots samples.     **
+	// ***********************************************************************
+
+	// ** AAD authentication settings: **
 	// TODO: Put your Service Principal (SP) details below. You'll need the Tenant ID (AAD Directory ID), Application/Client ID, and Application Secret/Password.
 	// Tip: You can generate an SP with the Azure CLI using a syntax like this and get the required info from the resulting JSON payload:
 	//      az ad sp create-for-rbac -n "http://aad-app-for-incremental-snapshots" --role contributor --scopes "/subscriptions/{my-subsccription-id}/resourceGroups/My-RG"
+	
 	utility::string_t aad_tenant_id(_XPLATSTR("AAD-Tenant-ID"));
 	utility::string_t aad_application_id(_XPLATSTR("AAD-Application-ID"));
 	utility::string_t aad_application_secret(_XPLATSTR("AAD-Application-Secret"));
 
+	// ** Incremental snapshot settings: **
+	// TODO: Put your subscription, resource group, region, snapshot name, and parent disk resource URI below.
+
+	utility::string_t subscription_id(_XPLATSTR("AZURE-SUBSCRIPTION-ID"));
+	utility::string_t resource_group_name(_XPLATSTR("RESOURCE-GROUP-NAME"));
+	utility::string_t region(_XPLATSTR("AZURE-REGION"));
+	utility::string_t snapshot_name(_XPLATSTR("SNAPSHOT-NAME"));
+	utility::string_t managed_disk_resource_uri(_XPLATSTR("subscriptions/[AZURE-SUBSCRIPTION-ID]/resourceGroups/[RESOURCE-GROUP-NAME]/providers/Microsoft.Compute/disks/[DISK-NAME]"));
+
+	// ***********************************************************************
+	// ** Settings below don't change that often and can be left untouched. **
+	// ***********************************************************************
+
 	// AAD resource and authority for authenticating against Service Principal or MSI.
-	utility::string_t aad_resource(_XPLATSTR("https://management.azure.com/"));
-	utility::string_t aad_authority(_XPLATSTR("https://login.microsoftonline.com/"));
+	utility::string_t aad_resource(_XPLATSTR("https://management.azure.com/")); // Public cloud
+	//utility::string_t aad_resource(_XPLATSTR("https://management.usgovcloudapi.net/")); // Gov cloud (Fairfax)
+	//utility::string_t aad_resource(_XPLATSTR("https://management.microsoftazure.de/")); // Germany (Black Forest)
+	//utility::string_t aad_resource(_XPLATSTR("https://management.chinacloudapi.cn/")); // China (Mooncake)
+
+	utility::string_t aad_authority(_XPLATSTR("https://login.microsoftonline.com/")); // Public cloud
+	//utility::string_t aad_authority(_XPLATSTR("https://login.microsoftonline.com/")); // Gov cloud (Fairfax)
+	//utility::string_t aad_authority(_XPLATSTR("https://login.microsoftonline.de/")); // Germany (Black Forest)
+	//utility::string_t aad_authority(_XPLATSTR("https://login.chinacloudapi.cn/")); // China (Mooncake)
 
 	// MSI (typically http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource= or the older http://localhost:50342/oauth2/token?resource=)
 	// Note: The resource query string parameter must be URI-encoded.
 	utility::string_t msi_endpoint(_XPLATSTR("http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource="));
-
 }}} // namespace azure::storage::samples
